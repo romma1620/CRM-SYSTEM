6284fa6fbd553033bdf176f4ebd1719ee74babd8
romma1620 <romankuchera@gmail.com>
2020-09-07 20:19:39 +0300

    fix v.0.3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 const PORT = process.env.PORT | 6161;
 
-app.listen(PORT, () => console.log(`Server has been started on ${PORT}`));
+app.listen(PORT, () => console.log(`Server has been started`));
 
